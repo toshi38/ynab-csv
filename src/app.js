e0ff9062d49d92ef28eb1e83c7bb86c7741efe77
Bjørn Thomsen <mail@bsthomsen.com>
2023-11-29 11:06:51 +0100

    Added support for danish letters

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // see http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
 // see http://stackoverflow.com/questions/18662404/download-lengthy-data-as-a-csv-file
 var encodings = [
-  "UTF-8", "IBM866", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5",
+  "UTF-8", "IBM866", "ISO-8859-1", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5",
   "ISO-8859-6", "ISO-8859-7", "ISO-8859-8", "ISO-8859-8-I", "ISO-8859-10",
   "ISO-8859-13", "ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "KOI8-R",
   "KOI8-U", "macintosh", "windows-874", "windows-1250", "windows-1251",
